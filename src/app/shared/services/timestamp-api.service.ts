a4fa1ac04c2a8ac1ea7c73c96f4fe98303524e3b
Josu Vazquez <josu9513@gmail.com>
2022-03-06 21:48:37 +0100

    Change API url  to enviroment

--- src/app/shared/services/timestamp-api.service.ts
+++ src/app/shared/services/timestamp-api.service.ts
@@ -1,20 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TimestampApiService {
-  api = 'http://worldtimeapi.org/api';
+  // api = 'http://worldtimeapi.org/api';
 
   constructor(private httpClient: HttpClient) { }
 
   getTimeZone(): Observable<any> {
-    return this.httpClient.get(`${this.api}/timezone`);
+    return this.httpClient.get(`${environment.backEndUrl}/timezone`);
   }
   
   getTimeStamp(timeZone: string): Observable<any> {
-    return this.httpClient.get(`${this.api}/timezone/${timeZone}`);
+    return this.httpClient.get(`${environment.backEndUrl}/timezone/${timeZone}`);
   }
 }
